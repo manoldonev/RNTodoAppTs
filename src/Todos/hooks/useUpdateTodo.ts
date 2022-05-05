6c307aad059c31246b0dde987c833dcffd39cbf7
Manol Donev <manol.donev@gmail.com>
2022-05-05 14:39:24 +0300

    refactor: query keys factory instead of hardcoded strings (#18)

--- src/Todos/hooks/useUpdateTodo.ts
+++ src/Todos/hooks/useUpdateTodo.ts
@@ -2,12 +2,13 @@ import type { UseMutationResult } from 'react-query';
 import { useQueryClient } from 'react-query';
 import type { UpdateTodoMutation, UpdateTodoMutationVariables } from '../../generated';
 import { useUpdateTodoMutation } from '../../generated';
+import { todoKeys } from './queryKeysFactory';
 
 const useUpdateTodo = (): UseMutationResult<UpdateTodoMutation, Error, UpdateTodoMutationVariables> => {
   const queryClient = useQueryClient();
 
   return useUpdateTodoMutation({
-    onSuccess: async () => queryClient.invalidateQueries(['Todos.infinite']),
+    onSuccess: async () => queryClient.invalidateQueries(todoKeys.all),
   });
 };
 
