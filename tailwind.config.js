2232074f9e02c511cdfc84a00d58c53cde4108e0
Manol Donev <manol.donev@gmail.com>
2022-05-12 14:47:17 +0300

    chore: add tailwindcss intellisense to vscode (#23)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,28 @@
-module.exports = { content: ['./src/**/*.{ts,tsx}'] };
+// NOTE: do not provide color values here -- use src/styling/themes instead
+// placeholder config file so tailwindCSS vscode extension provides correct intellisense for the overwridden theme color keys (i.e. bg-primary vs bg-red-100 / bg-red-200, etc.)
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '',
+      'on-primary': '',
+      'on-primary-variant': '',
+      'primary-container': '',
+      'on-primary-container': '',
+      secondary: '',
+      'on-secondary': '',
+      'secondary-container': '',
+      'on-secondary-container': '',
+      error: '',
+      'on-error': '',
+      'error-container': '',
+      'on-error-container': '',
+      background: '',
+      'on-background': '',
+      surface: '',
+      'on-surface': '',
+      outline: '',
+    },
+    extend: {},
+  },
+};
