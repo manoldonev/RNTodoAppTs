71618d0b735ae8d198b0b965a4cb4a32ae2aab2f
Manol Donev <manol.donev@gmail.com>
2022-05-12 14:40:46 +0300

    fix: light/dark theme typo

--- src/styling/themes.tsx
+++ src/styling/themes.tsx
@@ -2,29 +2,6 @@ import type { TwTheme } from 'twrnc/dist/esm/tw-config';
 
 export const LightTheme: TwTheme = {
   colors: {
-    primary: '#6ED2FF',
-    'on-primary': '#003549',
-    'on-primary-variant': '#003549D0',
-    'primary-container': '#004D68',
-    'on-primary-container': '#BEE8FF',
-    secondary: '#FFB4A2',
-    'on-secondary': '#650B00',
-    'secondary-container': '#8D1400',
-    'on-secondary-container': '#FFDAD1',
-    error: '#F2B8B5',
-    'on-error': '#601410',
-    'error-container': '#8C1D18',
-    'on-error-container': '#F9DEDC',
-    background: '#2C2C2C',
-    'on-background': '#E8E2D9',
-    surface: '#2C2C2C',
-    'on-surface': '#E8E2D9',
-    outline: '#938F99',
-  },
-};
-
-export const DarkTheme: TwTheme = {
-  colors: {
     primary: '#006688',
     'on-primary': '#FFFFFF',
     'on-primary-variant': '#FFFFFFD0',
@@ -43,5 +20,28 @@ export const DarkTheme: TwTheme = {
     surface: '#FFFBFE',
     'on-surface': '#1D1B16',
     outline: '#79747E',
+  },
+};
+
+export const DarkTheme: TwTheme = {
+  colors: {
+    primary: '#6ED2FF',
+    'on-primary': '#003549',
+    'on-primary-variant': '#003549D0',
+    'primary-container': '#004D68',
+    'on-primary-container': '#BEE8FF',
+    secondary: '#FFB4A2',
+    'on-secondary': '#650B00',
+    'secondary-container': '#8D1400',
+    'on-secondary-container': '#FFDAD1',
+    error: '#F2B8B5',
+    'on-error': '#601410',
+    'error-container': '#8C1D18',
+    'on-error-container': '#F9DEDC',
+    background: '#2C2C2C',
+    'on-background': '#E8E2D9',
+    surface: '#2C2C2C',
+    'on-surface': '#E8E2D9',
+    outline: '#938F99',
   },
 };
